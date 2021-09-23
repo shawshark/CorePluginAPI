eaa7c6b1d4a97c2cb89ac82c2246b81de14db044
Shawshark <chrissbolding@gmail.com>
2021-09-24 00:57:34 +1000

    uploading api v2

--- src/main/java/net/shawshark/core/api/CorePluginPlugin.java
+++ src/main/java/net/shawshark/core/api/CorePluginPlugin.java
@@ -1,10 +1,11 @@
 package net.shawshark.core.api;
 
 import net.shawshark.core.api.user.ICorePlayer;
+import org.bukkit.plugin.Plugin;
 
 import java.util.UUID;
 
-public interface CorePluginPlugin {
+public interface CorePluginPlugin extends Plugin {
     IPlayerDataManager getPlayerDataManager();
     ICorePlayer getCorePlayer(UUID uuid);
 }
